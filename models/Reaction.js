b799c67bae78bb9d2e9002200e1664094446fd32
Jacob Kachmarek <jacob.kachmarek@gmail.com>
2023-08-24 17:48:40 -0700

    finishing models

---
+++ models/Reaction.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const reactionSchema = new mongoose.Schema({
+    reactionId: {
+        type: mongoose.Schema.ObjectId,
+        default: new mongoose.Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (date) => date.toLocaleDateString()
+    }
+})
+
+module.exports = reactionSchema
--- models/Thought.js
+++ models/Thought.js
@@ -1,23 +1,40 @@
-const { Schema, model } = require('mongoose');
+const mongoose = require('mongoose')
+const reactionSchema = require('./Reaction')
 
-// const thoughtSchema = new Schema(
-//     {
-//         thoughtText: {
-//             type: String,
-//             required: true,
-//             maxlength: 280,
-//             minlength: 1,
-//         },
-//         createdAt: {
-//             type: Date,
-//             default: Date.now
-//         },
-//         username: {
-//             type: String,
-//             required: true,
-//         },
-//         reactions: {
+const thoughtSchema = new mongoose.Schema(
+    {
+        thoughtText: {
+            type: String, required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (date) => date.toLocaleDateString()
 
-//         }
-//     }
-// )
+        },
+        username: {
+            type: String, required: true
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    }
+)
+
+
+// setting virtual
+thoughtSchema.virtual('reactionCount').get(function () {
+    if (this.reactions) {
+        return this.reactions.length
+    } else {
+        return 0
+    }
+})
+
+const Thought = mongoose.model('thought', thoughtSchema)
+
+module.exports = Thought
