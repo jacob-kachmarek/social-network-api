83d3d7d6385997c3170051ee46bf20a953b47bff
Jacob Kachmarek <jacob.kachmarek@gmail.com>
2023-08-24 17:41:21 -0700

    creating models and setting up connection and server

---
+++ models/Thought.js
@@ -0,0 +1,23 @@
+const { Schema, model } = require('mongoose');
+
+// const thoughtSchema = new Schema(
+//     {
+//         thoughtText: {
+//             type: String,
+//             required: true,
+//             maxlength: 280,
+//             minlength: 1,
+//         },
+//         createdAt: {
+//             type: Date,
+//             default: Date.now
+//         },
+//         username: {
+//             type: String,
+//             required: true,
+//         },
+//         reactions: {
+
+//         }
+//     }
+// )
---
+++ models/User.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'user',
+            }
+        ]
+    },
+    {
+        toJSON: {
+            getters: true,
+        }
+    }
+)
+
+userSchema.virtual('friendCount').get(function () {
+    if (this.friends) {
+        return this.friends.length
+    } else {
+        return 0
+    }
+})
+
+const User = model('user', userSchema)
+
+module.exports = User
